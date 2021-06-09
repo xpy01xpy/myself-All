584f0f05a89a9936105fa094613151222778e408
965732851 <965732851@qq.com>
2021-06-09 16:23:57 +0800

    .

--- hello-vue3/src/App.vue
+++ hello-vue3/src/App.vue
@@ -1,37 +1,38 @@
 <!--
  * @Author: xpy
- * @Description: 
+ * @Description: 布局
  * @Date: 2021-06-08 19:21:10
- * @LastEditTime: 2021-06-08 20:52:27
+ * @LastEditTime: 2021-06-09 15:47:44
 -->
 <template>
   <div class="app-warpper">
-    <img alt="Vue logo" src="./assets/logo.png">
-    啊哈
+    <page-headers></page-headers>
   </div>
 </template>
 
 <script>
-
+import pageHeaders from './components/page-headers'
 export default {
   name: 'App',
   components: {
+    "page-headers":pageHeaders,
   }
 }
 </script>
 
 <style>
 #app {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  box-sizing: border-box;
-  border: 1px solid red;
+  height: 13.5rem;
 }
 .app-warpper{
-
+  font-size: 1rem;
+  border: 1px solid red;
+  height: 100%;
 }
 </style>
---
+++ hello-vue3/src/components/page-headers.vue
@@ -0,0 +1,60 @@
+<!--
+ * @Author: xpy
+ * @Description: 
+ * @Date: 2021-06-09 15:45:57
+ * @LastEditTime: 2021-06-09 16:19:22
+-->
+<template>
+  <div class="headers-warpper">
+    {{titleText}}
+    <div class="header-time">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"page-headers",
+  data(){
+    return {
+      titleText:'Data Visualization'
+    }
+  },
+  setup(props) {
+    console.log(`props`, props)
+    onMounted(() => {
+      console.log(`this.titleText`, this.titleText)
+      console.log(`titleText`, titleText)
+      console.log(`onMounted`)
+    })
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.headers-warpper{
+  height: 1.25rem;
+  width: 100%;
+  color: #ffffff;
+  font-size: .625rem;
+  font-weight: 500;
+  line-height: 1.125rem;
+  background: url(https://nichan-13.github.io/Echarts-Demo/images/head_bg.png) no-repeat;
+  background-size: 24rem 1.25rem;
+  text-align: center;
+  position: relative;
+}
+.header-time{
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  border: 1px solid red;
+  line-height: 1.125rem;
+  height: 1.25rem;
+  width: auto;
+  min-width: 1.25rem;
+}
+</style>
