57442fe04c71f1e7bc4cc877f1abfafc51a1ff2f
965732851 <965732851@qq.com>
2021-06-09 10:46:09 +0800

    .

---
+++ hello-vue3/src/App.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author: xpy
+ * @Description: 
+ * @Date: 2021-06-08 19:21:10
+ * @LastEditTime: 2021-06-08 20:52:27
+-->
+<template>
+  <div class="app-warpper">
+    <img alt="Vue logo" src="./assets/logo.png">
+    啊哈
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  box-sizing: border-box;
+  border: 1px solid red;
+}
+.app-warpper{
+
+}
+</style>
